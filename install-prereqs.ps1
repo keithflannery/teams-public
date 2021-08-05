650e2d669b2d282ea9797e432a9d5bf41f3aa432
Keith Flannery <kflannery@aus-it.com.au>
2021-08-05 12:33:08 +1000

    wip

---
+++ install-prereqs.ps1
@@ -0,0 +1,19 @@
+Write-Host "Checking for elevated permissions..."
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+            [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+    Break
+}
+else {
+    Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
+}
+
+$Installedmodules = Get-InstalledModule
+$modules = "MicrosoftTeams", "MSOnline", "AzureAD", "AZ", "ImportExcel"
+
+foreach ($checkModule in $modules) {
+    if ($Installedmodules.Name -notcontains $checkModule) {
+        "$checkModule not installed!"
+        Install-Module $checkModule -WhatIf
+    }
+}
