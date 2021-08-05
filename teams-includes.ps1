80ed7d16bdfff1cff207940eccfd09ff1067df48
Keith Flannery <kflannery@aus-it.com.au>
2021-08-05 14:33:43 +1000

    wip

--- teams-includes.ps1
+++ teams-includes.ps1
@@ -180,12 +180,8 @@ function new-kf-csuser {
 }
 
 function get-phonenumbers {
-    param (
-        [parameter()]
-        $objects = "UserPrincipalName, LineUri"
-    )
 
-    get-csonlineuser | where-object { $_.LineUri -match '^tel:\+61[2378]\d{8}(?:|;ext\=\d+)$' } | Select-Object $objects
+    get-csonlineuser | where-object { $_.LineUri -match '^tel:\+61[2378]\d{8}(?:|;ext\=\d+)$' } | Select-Object UserPrincipalName, LineUri
 }
 function validate-allusers {
     
